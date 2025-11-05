bde7db90d2dea5d88b9d484ce00703a4c2f5d219
Aman Porwal <amnmandliya786@gmail.com>
2025-11-06 03:07:50 +0530

    update paths

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -8,8 +8,8 @@ import {
   CardHeader,
   CardTitle,
 } from "../Components/ui/card"
-import { Input } from "./components/ui/input"
-import { Label } from "./components/ui/label"
+import { Input } from "../components/ui/input"
+import { Label } from "../components/ui/label"
 
 function LoginSignUp() {
   return (
